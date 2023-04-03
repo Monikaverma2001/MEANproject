e123a577f603ef081fdf6fc8f6a80561ca7ebef0
MonikaVerma2001 <monika8427084@gmail.com>
2023-04-03 16:08:29 +0530

    ok

--- models/student.js
+++ models/student.js
@@ -2,5 +2,7 @@ var mongoose = require('mongoose');
 // define our students model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Student', {
-name : {type : String, default: ''}
+name : {type : String, default: ''},
+password :{type : String, default: ''},
+position :{type : Boolean, default: false}
 });
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 console.log("ok");
 // set our port
 var mongoose = require('mongoose');
-const port = 3000;
+const port = 8080;
 // config files
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,7 +18,19 @@ mongoose . connect ( db . url ); //Mongoose connection created
 app.get('/', 
 
 //(req, res) => res.send('Welcome to Tutorialspoint!')
-(req, res) =>res.sendFile(__dirname + '/index.html')
+(req, res) =>res.sendFile(__dirname + '/view/home.html')
+
+);
+app.get('/login', 
+
+//(req, res) => res.send('Welcome to Tutorialspoint!')
+(req, res) =>res.sendFile(__dirname + '/view/index.html')
+
+);
+app.get('/logo', 
+
+//(req, res) => res.send('Welcome to Tutorialspoint!')
+(req, res) =>res.sendFile(__dirname + '/images/logo.jpg')
 
 );
 //defining route
@@ -34,26 +46,51 @@ async function getItems(){
     return Items;
   
   }
-app.get('/api/students', async (req, res) => {
+app.get('/faculity', async (req, res) => {
     getItems().then(function(FoundItems){
               
-        res.send(FoundItems);
+        res.json(FoundItems);
     
-    
+    });
 });
+app.post('/login', async (req, res) => {
+  let name=req.body.name;
+  let password=req.body.password;
+  let position=req.body.position;
+  Student.find({name,password,position}).then(function(FoundItems){
+        if(FoundItems==null)
+        {
+          res.sendFile(__dirname + '/view/home.html')
+        } 
+        else   
+        {
+         if(position==false)
+         {
+              res.sendFile(__dirname+'/view/mentorview/crud.html')
+         }
+         else{
+           res.sendFile(__dirname + '/view/hodview/crud.html');
+         }
+         
+        }
+  
+  });
 });
-app.post('/api/students/send', function (req, res) {
-  var student = new Student(); // create a new instance of the student model
-  student.name = "another"; // set the student name (comes from the request)
-  student.save(function(err) {
-  if (err)
-  res.send(err);
-  res.json({ message: 'student created!' });
+app.post('/search', async (req, res) => {
+  let name=req.body.name;
+  Student.findOne({name}).then(function(FoundItems){
+        if(FoundItems==null)
+        {
+          res.sendFile(__dirname + '/view/home.html')
+        } 
+        else   
+      res.sendFile(__dirname + '/view/hodview/crud.html');
+  
   });
-  });
+});
 
 
-app.post('/quotes', (req, res) => {
+app.post('/save', (req, res) => {
    // var student = new Student(); // create a new instance of the student model
 //   var item= req.body.name; //ody set the student name (comes from the request)
 //   student.save(item, function (err, result) {
@@ -63,11 +100,13 @@ app.post('/quotes', (req, res) => {
 // });
 // });
 const student= new Student({
-  name: req.body.name
+  name: req.body.name,
+   password:req.body.password,
+  position:req.body.position
 });
 
 student.save().then(()=>{
-  res.redirect('/api/students');
+  //res.redirect('/api/students');
 }).catch((err)=>{
   console.log("kuch err hai ");
   console.log(err);
@@ -96,6 +135,16 @@ app.delete('/quotes/:id', (req,res) => {
   //     });
   //   };
   // });
+app.get('/first',
+(req, res) =>res.sendFile(__dirname + '/images/first.jpeg')
+)
+app.get('/second',
+(req, res) =>res.sendFile(__dirname + '/images/second.jpg')
+)
+
+app.get('/third',
+(req, res) =>res.sendFile(__dirname + '/images/third.jpg')
+)
 
 
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+app.listen(port, ()=> console.log(`Example app listening on port http://localhost:${port}/`));
